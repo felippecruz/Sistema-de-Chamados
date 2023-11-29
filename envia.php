e7433058ef234470cd259dca2610f918b040e5c5
Felippe da Cruz Sousa <103589631+felippecruz@users.noreply.github.com>
2023-11-29 16:16:29 -0300

    Add files via upload
    em breve adicionarei as configurações do banco de dados

---
+++ envia.php
@@ -0,0 +1,16 @@
+<?php 
+require_once ('conecta.php');
+
+//CRIAR TABELA SQL
+$sql = "CREATE TABLE IF NOT EXISTS `logintela` 
+(`email` VARCHAR(50) NULL DEFAULT NULL , `senha` INT(20) NULL DEFAULT NULL ) ENGINE = InnoDB";
+mysqli_query($conn, $sql);
+
+//INSERIR DADOS NA TABELA
+$sql = "INSERT INTO logintela (email, senha) VALUES('', '')";
+if ($conn->query($sql)===TRUE){
+    echo "Dados inseridos na tabela com sucesso!";
+} else {
+    echo "Erro, favor preencha os dados corretamente" . $conn->error;
+    header('Location: ../systempainel.php');
+}
